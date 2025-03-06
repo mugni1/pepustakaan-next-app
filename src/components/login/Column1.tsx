bdca83210d25375b089d87c4bc77311ed1fc4c99
mugni1 <asepabdulmugni22@gmail.com>
2025-03-06 11:27:04 +0700

    modied

--- src/components/login/Column1.tsx
+++ src/components/login/Column1.tsx
@@ -1,11 +1,11 @@
 export default function Column1() {
   return (
-    <div className=" w-8/12 min-h-screen bglogin bg-center bg-cover">
+    <div className=" md:block hidden w-8/12 min-h-screen bglogin bg-center bg-cover">
       <div className="w-full h-full backdrop-blur-xs backdrop-brightness-50 flex items-center justify-center flex-col text-white px-20 gap-5">
         <h1 className="font-bold md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-6xl w-full text-start">
           Lorem ipsum dolor sit amet.
         </h1>
-        <p className=" md:text-base lg:text-lg xl:text-xl 2xl:text-2xl">
+        <p className=" md:text-sm lg:text-base xl:text-lg 2xl:text-2xl">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
           voluptas itaque expedita, error dolorum atque molestias fugit amet
           ratione necessitatibus optio delectus molestiae, sapiente, ducimus
--- src/components/login/MainContentLogin.tsx
+++ src/components/login/MainContentLogin.tsx
@@ -14,6 +14,7 @@ export default function MainContentLogin() {
   const [message, setMessage] = useState("");
   const [statusCode, setStatusCode] = useState(0);
   const [loadingBtn, setLoadingBtn] = useState(false);
+  const [isPassword, setIsPassword] = useState(true);
 
   const urlLogin: string = "http://localhost:8000/api/login";
   const urlLoginLogout: string = "http://localhost:8000/api/login-logout";
@@ -34,10 +35,10 @@ export default function MainContentLogin() {
         setIsNotify(true);
         setStatusCode(res.status);
       })
-      .catch((err) => {
-        setMessage(err.response.data.message);
+      .catch(() => {
+        setMessage("Email atau password yang anda masukan salah");
         setIsNotify(true);
-        setStatusCode(err.response.status);
+        setStatusCode(500);
       })
       .finally(() => {
         setLoadingBtn(false);
@@ -56,7 +57,7 @@ export default function MainContentLogin() {
   }, []);
 
   return (
-    <section className="w-full min-h-screen flex flex-wrap overflow-hidden relative">
+    <section className="flex w-full min-h-screen  flex-wrap overflow-hidden relative px-5 md:px-0">
       <NotifyError
         isNotify={isNotify}
         message={message}
@@ -66,8 +67,8 @@ export default function MainContentLogin() {
       <Column1 />
       {/* end column 1  */}
       {/* column 2  */}
-      <div className=" w-4/12 min-h-screen flex items-center justify-center">
-        <div className="w-full md:w-10/12 xl:w-8/12 2xl:w-1/2 mx-auto rounded-xl shadow-lg p-5 flex flex-col gap-5">
+      <div className="w-full md:w-4/12 min-h-screen flex items-center justify-center ">
+        <div className="w-full md:w-10/12 xl:w-8/12 2xl:w-1/2 mx-auto rounded-xl shadow-lg p-5 flex flex-col gap-5 border border-slate-500">
           <Title>SIGN IN</Title>
           <form
             onSubmit={handleSubmit}
@@ -82,14 +83,34 @@ export default function MainContentLogin() {
               onChange={(e) => setEmail(e.target.value)}
               required
             />
-            <input
-              type="password"
-              placeholder="Password"
-              className="py-1 px-3 rounded-xl border w-full border-slate-600"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
+            <div className=" w-auto h-fit relative flex items-center">
+              <input
+                type={isPassword ? "password" : "text"}
+                placeholder="Password"
+                className="py-1 px-3 rounded-xl border w-full border-slate-600 pe-8"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <span
+                onClick={() => setIsPassword(!isPassword)}
+                className="absolute right-2 h-full flex items-center cursor-pointer"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="size-5"
+                >
+                  <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+                  <path
+                    fillRule="evenodd"
+                    d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
+            </div>
             <div className="w-full flex items-center gap-1">
               <input
                 type="checkbox"
