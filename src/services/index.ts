83342e7042899cbecd6f83ebbb4b2ce31b379c23
mugni1 <asepabdulmugni22@gmail.com>
2025-03-08 21:49:00 +0700

    add books page

--- src/services/index.ts
+++ src/services/index.ts
@@ -9,3 +9,11 @@ export async function getCount(params: string) {
   });
   return res.json();
 }
+
+export async function getBooks() {
+  const res = await fetch(`${baseUrl}/books`, {
+    cache: "force-cache",
+    next: { revalidate: 60 * 60 },
+  });
+  return res.json();
+}
