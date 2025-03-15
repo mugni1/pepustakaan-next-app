5e39d1261b7442fdc6795aabb0e526820f448191
mugni1 <asepabdulmugni22@gmail.com>
2025-03-15 20:06:11 +0700

    add error page

--- src/app/(admin)/dashboard/member/loading.tsx
+++ src/app/(admin)/dashboard/member/loading.tsx
@@ -9,7 +9,7 @@ export default function Loading() {
         <div className="w-5/12 bg-slate-400 animate-pulse h-10 rounded-lg"></div>
       </section>
       {/* end search  */}
-      
+      <section className="w-full rounded-lg h-40 bg-slate-400 animate-pulse"></section>
     </MainContainer>
   );
 }
