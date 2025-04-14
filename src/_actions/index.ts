2b3f44d0854bb9a5c1402a810a79c09f18908b5c
mugni1 <asepabdulmugni22@gmail.com>
2025-04-14 22:06:57 +0700

    modified form and switch to server action

--- src/_actions/index.ts
+++ src/_actions/index.ts
@@ -1,6 +1,68 @@
 "use server";
 
-export const createBorrow = async (formData: FormData) => {
-  const dataBody = Object.fromEntries(formData.entries());
-  console.log(dataBody);
+import { revalidatePath } from "next/cache";
+import { cookies } from "next/headers";
+import { z } from "zod";
+
+const baseApiURL = process.env.NEXT_PUBLIC_BASE_API_URL;
+
+const borrowValidateSchema = z.object({
+  memberID: z.string().min(1, "Harap Pilih Anggota").transform(Number),
+  bookID: z.string().min(1, "Harap Pilih Buku").transform(Number),
+  returnDate: z.string().min(1, "Masukan Tanggal Pengembalian"),
+  dailyFine: z
+    .string()
+    .min(2, "Harap Masukan Denda Harian dengan benar")
+    .transform(Number),
+});
+export const createBorrow = async (
+  prevState: null | { status: string; message: string; Error?: unknown },
+  formData: FormData
+) => {
+  const token = (await cookies()).get("auth_token")?.value;
+  const dataBodyValidate = borrowValidateSchema.safeParse(
+    Object.fromEntries(formData.entries())
+  );
+  if (!dataBodyValidate.success) {
+    return {
+      status: "warning",
+      message: "Harap isi semua form dengan benar dan coba lagi",
+      Error: dataBodyValidate.error.flatten().fieldErrors,
+    };
+  }
+  try {
+    const res = await fetch(`${baseApiURL}/borrowings`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json", // penting untuk JSON
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        user_id: dataBodyValidate.data?.memberID,
+        book_id: dataBodyValidate.data?.bookID,
+        return_date: dataBodyValidate.data?.returnDate,
+        daily_fine: dataBodyValidate.data?.dailyFine,
+      }),
+    });
+    if (!res.ok) {
+      const text = await res.text();
+      const json = JSON.parse(text);
+      const message = json.message;
+      return {
+        status: "warning",
+        message: message,
+      };
+    }
+    revalidatePath("/dashboard/transaction-borrow/add");
+    return {
+      status: "success",
+      message: "Berhasil Meminjam buku",
+    };
+  } catch {
+    return {
+      status: "failed",
+      message: "Terjadi Kesalah",
+    };
+  }
 };
--- src/app/dashboard/(dataTable)/transaction-borrow/add/FormAdd.tsx
+++ src/app/dashboard/(dataTable)/transaction-borrow/add/FormAdd.tsx
@@ -1,7 +1,7 @@
 "use client";
 import BtnClick from "@/components/Admin/Button/BtnClick";
 import BtnHref from "@/components/Admin/Button/BtnHref";
-import { useEffect } from "react";
+import { useActionState, useEffect } from "react";
 import React from "react";
 import { toast } from "react-toastify";
 import { createBorrow } from "@/_actions";
@@ -24,63 +24,77 @@ export default function FormAdd({
   books: Book[];
   members: Member[];
 }) {
+  const [state, formAction] = useActionState(createBorrow, null);
+
   useEffect(() => {
-    toast.info("Omagaaaaaaaa");
-  }, []);
+    if (state?.status == "warning") {
+      toast.warning(state.message);
+    }
+    if (state?.status == "success") {
+      toast.success(state.message);
+    }
+    if (state?.status == "failed") {
+      toast.error(state.message);
+    }
+  }, [state]);
   return (
-    <form action={createBorrow}>
+    <form action={formAction}>
       {/* input  */}
       <section className="w-full grid grid-cols-2 gap-5 mb-5">
         {/* select members  */}
-        <select
-          required
-          className="py-1 px-2 outline-purple-600 border border-slate-400 rounded-md text-slate-600"
-          name="memberID"
-        >
-          <option value="" disabled>
-            - Pilih Anggota -
-          </option>
-          {members.map((member: Member, index: number) => (
-            <option key={index + member.id} value={member.id}>
-              {member.username}
-            </option>
-          ))}
-        </select>
+        <div className="w-full flex flex-col">
+          <select
+            className="py-1 px-2 outline-purple-600 border border-slate-400 rounded-md text-slate-600"
+            name="memberID"
+          >
+            <option value="">- Pilih Anggota -</option>
+            {members.map((member: Member, index: number) => (
+              <option key={index + member.id} value={member.id}>
+                {member.username}
+              </option>
+            ))}
+          </select>
+          <i className="text-xs text-red-500 mt-1">{state?.Error?.memberID}</i>
+        </div>
         {/* end select mebers  */}
         {/* select book  */}
-        <select
-          required
-          className="py-1 px-2 outline-purple-600 border border-slate-400 rounded-md text-slate-600"
-          name="bookID"
-        >
-          <option value="">- Pilih Buku -</option>
-          {books.map((book: Book, index) => (
-            <option key={index} value={book.id}>
-              {book.title} ({book.stock})
-            </option>
-          ))}
-        </select>
+        <div className="w-full flex flex-col">
+          <select
+            className="py-1 px-2 outline-purple-600 border border-slate-400 rounded-md text-slate-600"
+            name="bookID"
+          >
+            <option value="">- Pilih Buku -</option>
+            {books.map((book: Book, index) => (
+              <option key={index} value={book.id}>
+                {book.title} ({book.stock})
+              </option>
+            ))}
+          </select>
+          <i className="text-xs text-red-500 mt-1">{state?.Error?.bookID}</i>
+        </div>
         {/* end select book  */}
         <div className="w-full flex flex-col">
           <label htmlFor="returnDate">Taggal di kembalikan</label>
           <input
-            required
             className="py-1 px-2 outline-purple-600 border border-slate-400 rounded-md text-slate-600"
             type="date"
             id="returnDate"
             name="returnDate"
           />
+          <i className="text-xs text-red-500 mt-1">
+            {state?.Error?.returnDate}
+          </i>
         </div>
         <div className="w-full flex flex-col">
           <label htmlFor="dailyFine">Denda telat / hari</label>
           <input
-            required
             className="py-1 px-2 outline-purple-600 border border-slate-400 rounded-md text-slate-600"
             type="number"
             placeholder="30000"
             id="dailyFine"
             name="dailyFine"
           />
+          <i className="text-xs text-red-500 mt-1">{state?.Error?.dailyFine}</i>
         </div>
       </section>
       {/* end input  */}
