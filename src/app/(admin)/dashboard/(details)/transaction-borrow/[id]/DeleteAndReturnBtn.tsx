f0828e1db40c5ec1f827b4be34e2b8b21c815305
mugni1 <asepabdulmugni22@gmail.com>
2025-04-18 17:12:07 +0700

    add

--- src/app/(admin)/dashboard/(details)/transaction-borrow/[id]/DeleteAndReturnBtn.tsx
+++ src/app/(admin)/dashboard/(details)/transaction-borrow/[id]/DeleteAndReturnBtn.tsx
@@ -1,17 +1,38 @@
 "use client";
-import { returnBook } from "@/_actions";
+import { deleteBorrow, returnBook } from "@/_actions";
+import BtnClick from "@/components/Admin/Button/BtnClick";
+import { redirect } from "next/navigation";
 import React from "react";
+import { LuArrowLeftToLine, LuTrash } from "react-icons/lu";
+import { toast } from "react-toastify";
 import swal from "sweetalert";
 
 export default function DeleteAndReturnBtn({ id }: { id: number }) {
   async function handleReturnBook() {
     const res = await returnBook(id);
+    if (res.status == "failed") {
+      toast.error(res.message);
+    }
+    if (res.status == "success") {
+      toast.success(res.message);
+      redirect("/dashboard/transaction-borrow/");
+    }
+  }
+  async function handleDeleteBorrow() {
+    const res = await deleteBorrow(id);
+    if (res.status == "failed") {
+      toast.error(res.message);
+    }
+    if (res.status == "success") {
+      toast.success(res.message);
+      redirect("/dashboard/transaction-borrow/");
+    }
   }
   function handleReturn() {
     swal({
       icon: "warning",
       title: "Peringatan!",
-      text: "Apakah yakin mengembalikan buku ini?",
+      text: "Apakah yakin ingin mengembalikan buku ini?",
       dangerMode: true,
       buttons: ["Tidak", "Ya"],
     }).then((isTrue) => {
@@ -20,17 +41,34 @@ export default function DeleteAndReturnBtn({ id }: { id: number }) {
       }
     });
   }
+  function handleDelete() {
+    swal({
+      icon: "warning",
+      title: "Peringatan!",
+      text: "Apakah yakin ingin menghapus pinjaman ini?",
+      dangerMode: true,
+      buttons: ["Tidak", "Ya"],
+    }).then((isTrue) => {
+      if (isTrue) {
+        handleDeleteBorrow();
+      }
+    });
+  }
   return (
     <div className="flex gap-3">
-      <button className="font-bold text-background1 bg-red-500 px-5 rounded-md shadow-md cursor-pointer">
-        Hapus Pinjaman
-      </button>
-      <button
-        onClick={() => handleReturn()}
-        className="font-bold text-background1 bg-emerald-500 px-5 rounded-md shadow-md cursor-pointer"
+      <BtnClick
+        click={() => handleDelete()}
+        className="font-bold text-background1 bg-red-500 px-5 rounded-md shadow-md cursor-pointer flex items-center gap-1"
       >
-        Kembalikan Buku
-      </button>
+        <LuTrash size={20} />
+        Hapus
+      </BtnClick>
+      <BtnClick
+        click={() => handleReturn()}
+        className="font-bold text-background1 bg-emerald-500 px-5 rounded-md shadow-md cursor-pointer flex items-center gap-1"
+      >
+        <LuArrowLeftToLine size={20} /> Kembalikan
+      </BtnClick>
     </div>
   );
 }
