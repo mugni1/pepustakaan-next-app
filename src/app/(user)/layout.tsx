22b9413cc30832a3525b3dcb6d0b769ce4bb7bd1
mugni1 <asepabdulmugni22@gmail.com>
2025-04-20 19:33:40 +0700

    modified

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -11,8 +11,8 @@ export default function RootLayout({
     <>
       {children}
       <NavigationBar />
-      <footer className="w-full rounded-t-3xl bg-gradient-to-br from-accent1 to-accent2 text-background1 font-gabarito ">
-        <div className="container mx-auto px-5 grid md:grid-cols-3 grid-cols-1 py-5 gap-5">
+      <footer className="w-full rounded-t-2xl bg-gradient-to-br from-accent1 to-accent2 text-background1 font-gabarito ">
+        <div className="container mx-auto px-5 grid md:grid-cols-2 grid-cols-1 py-5 gap-5">
           <section className="flex flex-col gap-4">
             <h2 className="font-bold text-2xl">PUSTAKA</h2>
             <span>
@@ -31,14 +31,6 @@ export default function RootLayout({
               <Link href={"/profile"}>Profile</Link>
               <Link href={"/borrowings"}>Peminjaman</Link>
               <Link href={"/returns"}>Pengembalian</Link>
-            </div>
-          </section>
-          <section className="flex flex-col gap-4">
-            <h2 className="font-bold text-2xl">FAQ</h2>
-            <div className="flex flex-col">
-              <Link href={"/"}>Instagram</Link>
-              <Link href={"/"}>Facebook</Link>
-              <Link href={"/"}>Tiktok</Link>
             </div>
           </section>
         </div>
